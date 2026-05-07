486139a6427d15b1aeb0ef2bbd44e332f811dc53
Mykyta Huzhva <nekbrodiaga@gmail.com>
2026-05-08 01:00:49 +0300

    Minor fixes

--- ServiceContracts/DTO/UserResponse.cs
+++ ServiceContracts/DTO/UserResponse.cs
@@ -4,7 +4,5 @@
     {
         public Guid Id { get; set; }
         public string? Email { get; set; }
-        public string? PasswordHash { get; set; }
-        public List<TaskResponse>? Tasks { get; set; }
     }
 }
